2269e3e5e4ad2d0112624fbce06d62054fc09819
Dmitriy Obrazumov <obrazumovd@mail.ru>
2025-03-13 13:51:13 +0300

    Initial Commit

---
+++ TestTracks/TestTracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTracksApp.swift
+//  TestTracks
+//
+//  Created by Dmitriy Obrazumov on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
